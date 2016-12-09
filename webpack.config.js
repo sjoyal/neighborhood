d0137d42026c9e2d06b3ea9ac8d69745f7777ab7
Sean Joyal <sean.joyal@advent.com>
2016-12-09 17:13:10 -0500

    Shaving Yaks: updates to npm scripts and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,52 @@
-import path from 'path';
-import webpack from 'webpack';
+const path from 'path';
+const webpack from 'webpack';
 
 const node_dir = __dirname + '/node_modules';
 
 module.exports = {
-  resolve: {
-    alias: {
-      lodash: node_dir + '/lodash',
-      react: node_dir + '/react',
-      reactDome: node_dir + '/react-dom'
-    }
-  },
+    devtool: 'eval-source-map',
 
-  entry: {
-    app: ['babel-polyfill', './src/main'],
-    vendor: ['lodash', 'react', 'react-dom']
-  },
-
-  output: {
-    path: __dirname + '/dist',
-    filename: 'js/[name].bundle.js'
-  },
-
-  module: {
-    loaders: [
-      { test: \/.js?$/,
-        loader: 'babel-loader',
-        include: [
-          path.resolve(__dirname, 'src')
-        ],
-        query: {
-          plugins: ['babel-plugin-transform-decorators-legacy'],
-          presets: ['es2015', 'stage-1', 'react']
+    resolve: {
+        alias: {
+            react: node_dir + '/react',
+            reactDom: node_dir + '/react-dom',
         }
-      },
-      {
-        test: \/.scss$/,
-        loader: 'style-loader!css-loader!sass-loader'
-      }
+    },
+
+    entry: {
+        app: ['babel-polyfill', './src/main'],
+        vendor: ['react', 'react-dom'],
+    },
+
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: '[name].module.js',
+        publicPath: 'http://localhost:8080/dist/',
+    },
+
+    module: {
+        loaders: [
+            { test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    plugins: ['babel-plugin-transform-decorators-legacy'],
+                    presets: ['es2015', 'stage-1', 'react'],
+                }
+            },
+            {
+                test: \/.scss$/,
+                loader: 'style-loader!css-loader!sass-loader',
+            }
+        ]
+    }
+
+    devServer: {
+        contentBase: './src',
+        publicPath: 'http://localhost:8080/dist/',
+    }
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
     ]
-  }
-}
+};
